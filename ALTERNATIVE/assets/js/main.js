e8108cf3935d60f53cfaa4af161885c78deb3927
dev-prime99 <prime99@icloud.com>
2019-10-24 17:55:25 +0200

    added colors

--- ALTERNATIVE/assets/js/main.js
+++ ALTERNATIVE/assets/js/main.js
@@ -4,13 +4,13 @@ for (var i = 1; i < 51; i++) {
   // console.log(i);
   if ((i % 5 == 0) && (i % 3 == 0)){
     //console.log("FrizzBuzz");
-    document.getElementById('list').innerHTML += "<li>" + 'FrizzBuzz' + "</li>";
+    document.getElementById('list').innerHTML += '<li class="red">' + 'FrizzBuzz' + "</li>";
   } else if (i % 5 == 0){
     //console.log("Buzz");
-    document.getElementById('list').innerHTML += "<li>" + 'Buzz' + "</li>";
+    document.getElementById('list').innerHTML += '<li class="green">' + 'Buzz' + "</li>";
   } else if (i % 3 == 0){
     //console.log("Frizz")
-    document.getElementById('list').innerHTML += "<li>" + 'Frizz' + "</li>";
+    document.getElementById('list').innerHTML += '<li class="yellow">' + 'Frizz' + "</li>";
   } else{
     //console.log(i);
     document.getElementById('list').innerHTML += "<li>" + i + "</li>";
@@ -22,13 +22,13 @@ for (var ii = 51; ii < 101; ii++) {
   // console.log(i);
   if ((ii % 5 == 0) && (ii % 3 == 0)){
     //console.log("FrizzBuzz");
-    document.getElementById('list2').innerHTML += "<li>" + 'FrizzBuzz' + "</li>";
+    document.getElementById('list2').innerHTML += '<li class="red">' + 'FrizzBuzz' + "</li>";
   } else if (ii % 5 == 0){
     //console.log("Buzz");
-    document.getElementById('list2').innerHTML += "<li>" + 'Buzz' + "</li>";
+    document.getElementById('list2').innerHTML += '<li class="green">' + 'Buzz' + "</li>";
   } else if (ii % 3 == 0){
     //console.log("Frizz")
-    document.getElementById('list2').innerHTML += "<li>" + 'Frizz' + "</li>";
+    document.getElementById('list2').innerHTML += '<li class="yellow">' + 'Frizz' + "</li>";
   } else{
     //console.log(i);
     document.getElementById('list2').innerHTML += "<li>" + ii + "</li>";
